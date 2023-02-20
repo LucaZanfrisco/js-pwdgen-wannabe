9ce9a2ad810e824b0518334e95a518886af862b2
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-02-20 14:51:06 +0100

    add a little bit of style

--- js/main.js
+++ js/main.js
@@ -6,6 +6,11 @@ const favColor = prompt('Inserire il colore preferito');
 // Combinazione degli input per la generazione della password
 const password = `${firstName}${lastName}${favColor}89`;
 
+// Stampa in una tabella delle credenziali inserite
+document.getElementById('name').innerHTML = firstName;
+document.getElementById('surname').innerHTML = lastName;
+document.getElementById('favoriteColor').innerHTML = favColor;
+document.getElementById('pass').innerHTML = password;
+
 // Stampa della password 
-document.writeln(password);
 console.log(password);
